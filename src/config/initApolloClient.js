d252a5499612b76be05c9382a0a5e290585be5b2
xun <me@xun.com.my>
2019-11-05 21:52:39 +0800

    fix auth in apollo

--- src/config/initApolloClient.js
+++ src/config/initApolloClient.js
@@ -3,6 +3,7 @@ import { resolvers, typeDefs } from "graphql";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
 import { onError } from "apollo-link-error";
 import { ApolloLink } from "apollo-link";
 
@@ -17,7 +18,17 @@ if (!process.browser) {
  * Creates and configures the ApolloClient
  * @param  {Object} [initialState={}]
  */
-function createApolloClient(initialState = {}) {
+function createApolloClient(initialState = {}, { getToken }) {
+  const authLink = setContext((_, { headers }) => {
+    const token = getToken();
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : ""
+      }
+    };
+  });
+
   const cache = new InMemoryCache().restore(initialState);
   const client = new ApolloClient({
     ssrMode: typeof window === "undefined", // Disables forceFetch on the server (so queries are only run once)
@@ -35,6 +46,7 @@ function createApolloClient(initialState = {}) {
         // eslint-disable-next-line
         if (networkError) console.log(`[Network error]: ${networkError}`);
       }),
+      authLink,
       new HttpLink({
         uri: "https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn", // Server URL (must be absolute)
         credentials: "same-origin" // Additional fetch() options like `credentials` or `headers`
@@ -56,16 +68,16 @@ function createApolloClient(initialState = {}) {
  * Creates or reuses apollo client in the browser.
  * @param  {Object} initialState
  */
-export default function initApolloClient(initialState) {
+export default function initApolloClient(initialState, options) {
   // Make sure to create a new client for every server-side request so that data
   // isn't shared between connections (which would be bad)
   if (typeof window === "undefined") {
-    return createApolloClient(initialState);
+    return createApolloClient(initialState, options);
   }
 
   // Reuse client on the client-side
   if (!apolloClient) {
-    apolloClient = createApolloClient(initialState);
+    apolloClient = createApolloClient(initialState, options);
   }
 
   return apolloClient;
