6254475e80b4164f020f5bac5c907fb18473a1a0
Joshua <joshua@lavax.co>
2019-11-15 21:43:06 +0800

    updated app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
-import React, { Component } from "react";
-import HomeScreen from "screens/Home";
+import { Component } from "react";
+import redirect from "config/redirect";
 
 import { withTranslation } from "i18next";
 
 class Index extends Component {
+  componentDidMount() {
+    const { authUser } = this.props;
+    if (!authUser) {
+      redirect({}, "/signin");
+    }
+  }
+
   getInitialProps = () => ({
     namespacesRequired: ["common"]
   });
 
   render() {
-    return <HomeScreen />;
+    return null;
   }
 }
 
