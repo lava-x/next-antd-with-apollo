693098149171ab2d01ac24fac080c10cadcc0b35
Joshua <joshua@lavax.co>
2019-11-15 21:39:36 +0800

    updated header component

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,62 +1,76 @@
 import React, { Component } from "react";
-// import Link from 'next/link';
-import { Link } from "i18next";
-import ActiveLink from "components/ActiveLink";
+import Link from "next/link";
+import _ from "lodash";
+import classnames from "classnames";
+import { Dropdown, Menu, Icon, Button, Avatar } from "antd";
 import LogoDark from "assets/Logo.svg";
 import styles from "./styles";
 
 class Header extends Component {
+  onHandleMenuClick = e => {
+    switch (e.key) {
+      case "signout": {
+        this.props.onActionSignOut();
+        break;
+      }
+      default:
+        break;
+    }
+  };
+
   render() {
-    return (
-      <>
-        <nav className="navbar">
-          <div className="container">
-            <div className="navbar-brand">
-              <Link href="/">
-                <a className="navbar-item">
-                  <img
-                    src={LogoDark}
-                    alt="LavaX Technologies Sdn Bhd"
-                    width="112"
-                    height="28"
-                  />
-                </a>
-              </Link>
-              <div className="navbar-burger burger">
-                <span />
-                <span />
-                <span />
-              </div>
-            </div>
-            <div className="navbar-menu">
-              <div className="navbar-end">
-                <ActiveLink
-                  href="/todo"
-                  activeClass="active"
-                  className="navbar-item"
-                  alt="Todo"
-                  title="Todo"
-                />
-                <ActiveLink
-                  href="/test"
-                  activeClass="active"
-                  className="navbar-item"
-                  alt="test page"
-                  title="Test Page"
-                />
-                <ActiveLink
-                  href="/translate"
-                  activeClass="active"
-                  className="navbar-item"
-                  alt="Translate page"
-                  title="Translate Page"
-                />
-              </div>
-            </div>
-          </div>
-        </nav>
+    const { authUser, mode } = this.props;
+    // if there is default avatar, can replace the null to image link
+    const avatar = authUser && authUser.avatar ? authUser.avatar : null;
+    // To check if there is first Name and Last Name for that User
+    // U may replace the entry for your authUser firstname and lastname
+    const hasAuthUser = !_.isEmpty(authUser);
+    const hasFirstName = hasAuthUser && !_.isEmpty(authUser.firstName);
+    const hasLastName = hasAuthUser && !_.isEmpty(authUser.lastName);
+
+    const menu = (
+      <Menu onClick={this.onHandleMenuClick}>
+        <Menu.Item key="signout">
+          <Icon type="logout" />
+          Signout
+        </Menu.Item>
         <style jsx>{styles}</style>
-      </>
+      </Menu>
+    );
+
+    return (
+      <div className="ts-layout-header clearfix">
+        <div
+          className={classnames("logo", { "text-center": mode === "desktop" })}
+          span={22}
+        >
+          <img
+            src={LogoDark}
+            alt="LavaX Technologies Sdn Bhd"
+            width="112"
+            height="28"
+          />
+        </div>
+
+        <div className="header-actions">
+          {authUser ? (
+            <Dropdown trigger={["click"]} overlay={menu}>
+              <a className="d-inline-block">
+                <Avatar src={avatar} size={40} />
+                <span className="d-inline-block ml10">
+                  {hasFirstName ? authUser.firstName : "Admin"}{" "}
+                  {hasLastName ? authUser.lastName : null}
+                </span>
+              </a>
+            </Dropdown>
+          ) : (
+            <Link href="/signin">
+              <Button type="primary">Sign In</Button>
+            </Link>
+          )}
+        </div>
+        <style jsx>{styles}</style>
+      </div>
     );
   }
 }
